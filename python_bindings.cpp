0847f2435b530b150b0b501af879f334122d0f9c
Adamiscool123 <adamidrissi177@gmail.com>
2026-04-23 16:39:57 +0100

    Changed pybinding

--- python_bindings.cpp
+++ python_bindings.cpp
@@ -53,34 +53,24 @@ PYBIND11_MODULE(orderbook_wrapper, m) {
         .def("sell", &Order_Book::sell)
         .def("buy", &Order_Book::buy);
 
-    // 1. Wrap the Base Class
-    py::class_<Agent_Base>(m, "AgentBase")
-        .def("infinite_loop", &Agent_Base::infinite_loop)
-        .def("execute", &Agent_Base::execute)
-        .def("loop", &Agent_Base::loop);
-
     py::class_<manual, Agent_Base>(m, "Manual")
-        .def(py::init<Global_Variables&>())
+        .def(py::init<Global_Variables&, int>())
         .def("trade", &manual::trade)
         .def("execute_agent", &manual::execute_agent);
 
-    // 3. Wrap 'market_maker'
     py::class_<market_maker, Agent_Base>(m, "MarketMaker")
-        .def(py::init<Global_Variables&>())
+        .def(py::init<Global_Variables&, int>())
         .def("execute_agent", &market_maker::execute_agent);
 
-    // 4. Wrap 'noise_trader'
     py::class_<noise_trader, Agent_Base>(m, "NoiseTrader")
-        .def(py::init<Global_Variables&>())
+        .def(py::init<Global_Variables&, int>())
         .def("execute_agent", &noise_trader::execute_agent);
 
-    // 5. Wrap 'trend_follower'
     py::class_<trend_follower, Agent_Base>(m, "TrendFollower")
-        .def(py::init<Global_Variables&>())
+        .def(py::init<Global_Variables&, int>())
         .def("execute_agent", &trend_follower::execute_agent);
 
-    // 6. Wrap 'whale'
     py::class_<whale, Agent_Base>(m, "Whale")
-        .def(py::init<Global_Variables&>())
+        .def(py::init<Global_Variables&, int>())
         .def("execute_agent", &whale::execute_agent);
 }
